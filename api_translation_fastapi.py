bd66e76a87d5f1665823bbd670f57112c18a25ac
Emanuel <129886641+Emanuel0428@users.noreply.github.com>
2025-01-21 23:05:35 -0500

    Update api_translation_fastapi.py

--- api_translation_fastapi.py
+++ api_translation_fastapi.py
@@ -12,10 +12,8 @@ app = FastAPI()
 translator = Translator()
 templates = Jinja2Templates(directory="templates")
 
-# Montar la carpeta estática para los estilos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Crear carpeta de archivos temporales si no existe para guardar los archivos temporalmente por la sesion
 TEMP_FOLDER = "temp_files"
 os.makedirs(TEMP_FOLDER, exist_ok=True)
 
@@ -42,23 +40,22 @@ async def translate_file(
     else:
         return {"error": "Tipo de archivo no soportado :/. Por favor sube un archivo .pdf o .docx."}
 
-# Función para traducir un archivo PDF
 async def translate_pdf_to_docx(file: UploadFile, src_lang: str, dest_lang: str):
-    # Guardar archivo PDF temporalmente
+    
     input_pdf_path = os.path.join(TEMP_FOLDER, f"temp_{file.filename}")
     with open(input_pdf_path, "wb") as temp_file:
         temp_file.write(await file.read())
 
-    # Convertir PDF a DOCX para usarlo
+    
     output_docx_path = os.path.join(TEMP_FOLDER, f"temp_{file.filename.rsplit('.', 1)[0]}.docx")
     cv = Converter(input_pdf_path)
     cv.convert(output_docx_path, start=0, end=None)
     cv.close()
 
-    # Traducir el archivo DOCX generado a partir del PDF
+   
     translated_docx_path = await translate_docx_internal(output_docx_path, src_lang, dest_lang)
 
-    # Eliminar archivos temporales generados durante el proceso
+    
     os.remove(input_pdf_path)
     os.remove(output_docx_path)
 
@@ -68,17 +65,15 @@ async def translate_pdf_to_docx(file: UploadFile, src_lang: str, dest_lang: str)
         filename=f"translated_{file.filename.rsplit('.', 1)[0]}.docx",
     )
 
-# Función para traducir un archivo DOCX
+
 async def translate_docx(file: UploadFile, src_lang: str, dest_lang: str):
-    # Guardar archivo DOCX temporalmente para usarlo
+    
     input_docx_path = os.path.join(TEMP_FOLDER, f"temp_{file.filename}")
     with open(input_docx_path, "wb") as temp_file:
         temp_file.write(await file.read())
 
-    # Traducir el archivo DOCX y obtener la ruta del archivo traducido
     translated_docx_path = await translate_docx_internal(input_docx_path, src_lang, dest_lang)
 
-    # Eliminar archivo original temporal generado durante el proceso
     os.remove(input_docx_path)
 
     return FileResponse(
@@ -87,7 +82,6 @@ async def translate_docx(file: UploadFile, src_lang: str, dest_lang: str):
         filename=f"translated_{file.filename}",
     )
 
-# Función para traducir el contenido de un archivo DOCX
 async def translate_docx_internal(input_docx_path, src_lang, dest_lang):
 
     document = Document(input_docx_path)
@@ -96,13 +90,11 @@ async def translate_docx_internal(input_docx_path, src_lang, dest_lang):
 
     os.makedirs(os.path.dirname(translated_docx_path), exist_ok=True)
 
-    # Traducir el contenido
     for paragraph in document.paragraphs:
         if paragraph.text.strip():
             translated_text = translator.translate(paragraph.text, src=src_lang, dest=dest_lang).text
             paragraph.text = translated_text
 
-    # Guardar el archivo traducido
     document.save(translated_docx_path)
 
     return translated_docx_path
